6c3c3535ee1cb0b33aea6bbda15afc07095dc94c
TimmosQuadros <timm.rasmussen@gmail.com>
2020-07-23 15:17:37 +0200

    Try to add sound when new message arives

--- index.php
+++ index.php
@@ -49,6 +49,8 @@ if (!isset($_SESSION['name'])) {
 	<script type="text/javascript">
 		// jQuery Document
 		$(document).ready(function() {
+			var modifiedTS = 0;
+			var lastModifiedTS = 0;
 			//If user wants to end session
 			$("#exit").click(function() {
 				var exit = confirm("Are you sure you want to end the session?");
@@ -63,26 +65,32 @@ if (!isset($_SESSION['name'])) {
 					text: clientmsg
 				});
 				$("#usermsg").attr("value", "");
-				var audio = new Audio('message.mp3');
-				audio.play();
+				modifiedTS = filemtime("log.html");
 				return false;
 			});
 			function loadLog() {
-				var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height before the request
-				$.ajax({
-					url: "log.html",
-					cache: false,
-					success: function(html) {
-						$("#chatbox").html(html); //Insert chat log into the #chatbox div	
-						//Auto-scroll			
-						var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height after the request
-						if (newscrollHeight > oldscrollHeight) {
-							$("#chatbox").animate({
-								scrollTop: newscrollHeight
-							}, 'normal'); //Autoscroll to bottom of div
-						}
-					},
-				});
+				if(lastModifiedTS == modifiedTS){
+					return;
+				}else{
+					lastModifiedTS = modifiedTS;
+					var audio = new Audio('message.mp3');
+					audio.play();
+					var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height before the request
+					$.ajax({
+						url: "log.html",
+						cache: false,
+						success: function(html) {
+							$("#chatbox").html(html); //Insert chat log into the #chatbox div	
+							//Auto-scroll			
+							var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20; //Scroll height after the request
+							if (newscrollHeight > oldscrollHeight) {
+								$("#chatbox").animate({
+									scrollTop: newscrollHeight
+								}, 'normal'); //Autoscroll to bottom of div
+							}
+						},
+					});
+				}
 			}
 			setInterval(loadLog, 2500);
 		});
